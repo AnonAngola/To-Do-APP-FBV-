784eeb04fd7b85cd6acb2b8d2047caf0f556a37c
Kahilo Pedro <pedrojogad1o@gmail.com>
2023-02-28 22:33:34 +0100

    deploy adjustments

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n%&&9(^#hm=gor83fx)!m5q7(o+vqn2ar$j-owy$4)0(ev!bpd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
